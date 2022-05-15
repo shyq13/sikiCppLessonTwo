3e0fa1b8d2e3c41136e34926e0d3503c95993c03
shyq13 <2700201313@qq.com>
2022-05-15 18:06:37 +0800

    添加项目文件。

--- lesson1/lesson1（3）/lesson1（3）.cpp
+++ lesson1/lesson1（3）/lesson1（3）.cpp
@@ -4,7 +4,7 @@
 //运行该项目时一定要将它设为启动项！！！
 using namespace std;
 
-int main(){
+int main() {
 	// C++第一季复习（4）
 	// 1. if语句
 	int a = 10, b = 20;
@@ -17,6 +17,91 @@ int main(){
 	else {
 		cout << "a < b" << endl;
 	}
+
+	cout << '\n'
+		<< "------------------------------------"
+		<< endl << '\n';
+
+	// 2. switch语句
+	int vip;
+	cout << "请输入您的会员等级：";
+	cin >> vip;
+	switch (vip) {
+	case 1:
+		cout << "您的会员等级是：普通会员" << endl;
+		break;
+	case 2:
+		cout << "您的会员等级是：银卡会员" << endl;
+		break;
+	case 3:
+		cout << "您的会员等级是：金卡会员" << endl;
+		break;
+	case 4:
+		cout << "您的会员等级是：白金会员" << endl;
+		break;
+	case 5:
+		cout << "您的会员等级是：铂金会员" << endl;
+		break;
+	case 6:
+		cout << "您的会员等级是：钻石会员" << endl;
+		break;
+	default:
+		break;
+	}
+
+	cout << '\n'
+		<< "------------------------------------"
+		<< endl << '\n';
+
+	// 3. for循环
+	// 普通for循环
+	for (int i = 0; i < 10; i++) {
+		cout << "现在i的值是" << i << endl;
+	}
+	cout << endl;
+
+	// 遍历
+	int arr[5] = { 1,2,3,4,5 };
+	cout << "遍历数组arr： { ";
+	for (int j = 0; j < 5; j++) {
+		if (j <= 4)
+			cout << arr[j] << ",";
+		else
+			cout << arr[j] << " ";
+	}
+	cout << "}" << endl << endl;
+
+	// 也可以这样遍历
+	cout << "第二种方式遍历数组arr： { ";
+	for (int k : arr) {
+		if (k <= 4)
+			cout << k << ",";
+		else
+			cout << k << " ";
+	}
+	cout << "}" << endl << endl;
+	
+	cout << '\n'
+		<< "------------------------------------"
+		<< endl << '\n';
+	
+	// 4. while循环
+	int num1 = 30, num2 = 15;
+	while (num1 > num2) {
+		cout << "Hello world!" << endl;
+		num1--;
+	}
+
+	cout << '\n'
+		<< "------------------------------------"
+		<< endl << '\n';
+
+	// 5. do while循环
+	int num3 = 30, num4 = 15;
+	do{
+		cout << "Hello C++!" << endl;
+		num3--;
+	} while (num3 > num4);
 	
 	
     return 0;
---
+++ lesson1Review/lesson1Review（0）/lesson1Review（0）.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <string>
+// 运行该项目时一定要将它设为启动项！！！
+// 运行该项目时一定要将它设为启动项！！！
+// 运行该项目时一定要将它设为启动项！！！
+using namespace std;
+
+struct GameData {
+	string name;
+	double score;
+};
+
+int main(){
+	// 任务：让用户输入5个游戏的数据，并根据游戏评分高低进行排序，如果评分相等
+	// 则按录入顺序进行排序。最后，将数据按从高到低（评分）的顺序输出
+	// 输出格式： 游戏排名：游戏名称（评分）
+	const int size = 5;
+	GameData gameData[size] = {};
+	cout << "请输入5个你喜欢的游戏的名称及评分（0-10之间的小数）" << endl;
+	for (int i = 0; i < 5; i++) {
+		cout << "请输入第" << i + 1 << "个游戏的名称：";
+		cin >> gameData[i].name;
+		cout << "请输入第" << i + 1 << "个游戏的评分：";
+		cin >> gameData[i].score;
+		cout << '\n'
+			<< "---------------------------------------------"
+			<< endl << '\n';
+	}
+	int max = 0;
+	string maxName= "";
+	for (int i = 0; i < size - 1; i++) {
+		for (int j = i + 1; j < size; j++) {
+			if (gameData[i].score < gameData[j].score) {
+				max = gameData[i].score;
+				gameData[i].score = gameData[j].score;
+				gameData[j].score = max;
+				max = 0;
+				maxName = gameData[i].name;
+				gameData[i].name = gameData[j].name;
+				gameData[j].name = maxName;
+				maxName = "";
+			}
+		}
+	}
+	for (int i = 0; i < size; i++) {
+		cout << "第" << i + 1 << "名" << " " << gameData[i].name 
+			<< "(" << gameData[i].score << "分)" << endl;
+	}
+
+	
+    return 0;
+}
